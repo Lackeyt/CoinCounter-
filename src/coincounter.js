fffc6601beed82e63c368c85f17f717825cfe3cb
Ian Gregg <iangregg188@gmail.com>
2020-08-31 14:24:54 -0700

    finished the content for part 2.
    Co-authored-by: James Henager <jameschenager@gmail.com>
    co-authored-by:  Tyson Lackey <lackeyt90@gmail.com>

--- src/coincounter.js
+++ src/coincounter.js
@@ -1,17 +1,18 @@
-export function coinCounter(input){ 
+//Recursion
+export function coinCounter(input){     //input = 4.99
   if (isNaN(input)) {
     return;
   }
 
   if(input >= .25){
-    return coinCounter(input % .25) + "Quarters: " + Math.floor(input/.25)
+    return coinCounter(input % .25) + "Quarters: " + Math.floor(input/.25)   //input % .25 = .24
   }
 
   if(input >= .10 && input < .25){
-    return coinCounter(input % .10) + "Dimes: " + Math.floor(input/.10) + ", "
+    return coinCounter(input % .10) + "Dimes: " + Math.floor(input/.10) + ", "  //input % .10 = .04
   }
 
-  if(input >= .5 && input <.10){
+  if(input >= .5 && input < .10){
     return coinCounter(input % .5) + "Nickles: " + Math.floor(input/.5) + ", "
   }
 
@@ -19,3 +20,19 @@ export function coinCounter(input){
     return "Pennies: " + Math.round((input * 100)) + ", "
   }
 }
+
+
+//Closure
+function coinCount(coin){
+  return function(amount){
+    return Math.floor(amount/coin)
+  }
+}
+
+//Variable functions
+const howManyQuarters = coinCount(.25)
+const howManyDimes = coinCount(.10)
+const howManyNickles = coinCount(.05)
+const howManyPennies = coinCount(.01)
+
+howManyQuarters(4.99) = 19
