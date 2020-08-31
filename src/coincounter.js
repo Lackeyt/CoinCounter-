d67b704ddb88c549db76e09abb7adf489fc70a5c
Ian Gregg <iangregg188@gmail.com>
2020-08-31 14:55:14 -0700

    working on refactoring the code inthe js logic.
    This application will take in currency number in (USD) and convert it into change needed in quarters, dimes, nickels and pennies.
    
    Co-authored-by: James Henager <jameschenager@gmail.com>
    co-authored-by:  Tyson Lackey <lackeyt90@gmail.com>

--- src/coincounter.js
+++ src/coincounter.js
@@ -1,32 +1,37 @@
 //Recursion
-export function coinCounter(input){     //input = 4.99
+export function coinCounter(input){
   if (isNaN(input)) {
     return;
   }
 
   if(input >= .25){
-    return coinCounter(input % .25) + "Quarters: " + Math.floor(input/.25)   //input % .25 = .24
+    return coinCounter(input % .25).concat(["Quarters: " + Math.floor(input/.25)])
   }
 
   if(input >= .10 && input < .25){
-    return coinCounter(input % .10) + "Dimes: " + Math.floor(input/.10) + ", "  //input % .10 = .04
+    return coinCounter(input % .10).concat(["Dimes: " + Math.floor(input/.10)])
   }
 
   if(input >= .5 && input < .10){
-    return coinCounter(input % .5) + "Nickles: " + Math.floor(input/.5) + ", "
+    return coinCounter(input % .5).concat(["Nickles: " + Math.floor(input/.5)])
   }
 
   if(input >= 0 && input < .05){
-    return "Pennies: " + Math.round((input * 100)) + ", "
+    return ["Pennies: " + Math.round((input * 100))]
   }
 }
+
+// ["Pennies: 4", "Dimes: 2", "Quarters: 19"]
 
 
 //Closure
 function coinCount(coin){
   return function(amount){
-    if()
-    return Math.floor(amount/coin)
+    if(amount % coin != 0){
+      return Math.floor(amount/coin) + "with" + (amount % coin) + "remaining"
+    } else {
+      return Math.floor(amount/coin)
+    }
   }
 }
 
@@ -35,5 +40,3 @@ const howManyQuarters = coinCount(.25)
 const howManyDimes = coinCount(.10)
 const howManyNickles = coinCount(.05)
 const howManyPennies = coinCount(.01)
-
-howManyQuarters(4.99) = 19
